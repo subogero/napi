931461c8a0599f5b299c3287f5ce7bc6d2d8a216
SZABO Gergely <szg@subogero.com>
2011-07-23 01:24:38 +0200

    napi keres running on OpenWRT perl

--- keres.pl
+++ keres.pl
@@ -1,17 +1,19 @@
-#!/usr/bin/microperl
+#!/usr/bin/perl
 print <<EOF;
 Content-type: text/html
 
 <head><title>keress a napiban</title></head>
 <body>
 <hr><h1 align="center">keress a napiban</h1><hr>
-<form method="GET" action="keres.pl">
+<form method="POST" action="keres.pl">
 <input type="text" name="rajz"/>
 </form><hr>
 EOF
-if ($ENV{QUERY_STRING} =~ /^rajz=(\w{1,40}).*$/) {
+$query .= $_ while (<>);
+($PWD = $0) =~ s/^(.+)keres\.pl$/$1/;
+if ($query =~ /^rajz=(\w{1,40}).*$/) {
     my $minta = $1;
-    open LS, "ls -1 |" or die "$!";
+    open LS, "ls -1 $PWD |" or die "$!";
     while (<LS>) {
         if (/^(\w*$minta\w*)\.[jpeg]{3,4}/i) {
             print "<a href=\"$_\">$1</a><br>";
