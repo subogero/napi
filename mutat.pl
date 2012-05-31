f3e4848dbaff51af3edc4dc38a7bde921be7ab2a
SZABÓ Gergely <szg@subogero.com>
2012-05-31 20:46:24 +0200

    mutat.pl fix title and error messages #4

--- mutat.pl
+++ mutat.pl
@@ -1,28 +1,32 @@
 #!/usr/bin/perl
-# Header and query form
-print <<HEADER;
-Content-type: text/html
-
-<html><head><title>keress a napiban</title></head><body>
-<a href="index.html">napi</a>
-<a href="tegnapi.html">tegnapi</a>
-<a href="komedia.html">kom&eacute;dia</a>
-<a href="keres.pl">keres</a>
-HEADER
-
 # Find working directory (uhttpd runs this in docroot)
 ($PWD = $0) =~ s/^(.+)mutat\.pl$/$1/;
 
 # Read picture name from QUERY_STRING env var (GET method)
 my $query = $ENV{QUERY_STRING};
 if ($query =~ /^rajz=(([\w]{5,40})\.jpe?g)$/) {
-    my ($filename, $basename) = ($1, $2);
+    ($filename, $basename) = ($1, $2);
     if (-f "$PWD$filename") {
-        print "$basename<hr><img src=\"$filename\" alt=\"$filename\"/>\n";
+        $stuff = "$basename<hr><img src=\"$filename\" alt=\"$filename\"/>\n";
     } else {
-        print "<hr>Itt bizony nincs $filename.\n";
+        $stuff = "<hr>404 - Itt nincs semmilyen $filename.\n";
     }
+} else {
+    $stuff = "<hr>Mit mutassak?";
 }
+
+# Header
+print <<HEADER;
+Content-type: text/html
+
+<html><head><title>napi $basename</title></head><body>
+<a href="index.html">napi</a>
+<a href="tegnapi.html">tegnapi</a>
+<a href="komedia.html">kom&eacute;dia</a>
+<a href="keres.pl">keres</a>
+HEADER
+
+print $stuff;
 
 # Footer
 print <<FOOTER;
