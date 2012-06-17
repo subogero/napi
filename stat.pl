d9f0c67240947d795329fcd179510af55142c6dc
SZABÓ Gergely <szg@subogero.com>
2012-06-17 18:55:33 +0200

    stat.pl shows monthly stats via html5 canvas

---
+++ stat.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+print <<HEADER;
+Content-type: text/html
+
+<!DOCTYPE html>
+<html><head>
+<title>napi statisztika</title>
+</head><body>
+<a href="index.html">napi</a>
+<a href="tegnapi.html">tegnapi</a>
+<a href="komedia.html">kom&eacute;dia</a>
+<a href="aindex.html">aindex</a>
+<a href="keres.pl">keres</a>
+<hr><h3 align="center">napi statisztika</h3><hr>
+HEADER
+
+open STATM, "stat.txt";
+$statm{ehavi} = <STATM>;
+$max = $statm{ehavi  };
+open STATM, "statm.txt";
+while (<STATM>) {
+    /^(.+) (.+)\n$/;
+    $statm{$1} = $2;
+    $max = $2 if ($max < $2);
+}
+close STATM;
+$h = keys(%statm) * 10;
+$max += 70;
+
+print <<CANVAS1;
+<canvas id="vaszon" width="$max" height="$h">
+Ez egy html5 canvas. Ainternetekszplorered meg egy elavult szar.
+</canvas>
+<script type="text/javascript">
+var c=document.getElementById("vaszon");
+var ctx=c.getContext("2d");
+ctx.font="8px monospace";
+CANVAS1
+
+$i = 0;
+foreach (reverse sort keys %statm) {
+print <<LINE;
+ctx.textAlign='start'; ctx.fillText("$_"        ,  0, $i*10+7);
+ctx.textAlign='right'; ctx.fillText("$statm{$_}", 60, $i*10+7);
+ctx.fillRect(70, $i*10, $statm{$_}, 8);
+LINE
+$i++;
+}
+
+print <<CANVAS2;
+</script>
+CANVAS2
+
+print <<FOOTER;
+<br><small><a href="http://github.com/subogero">github.com/subogero</a></small>
+</body></html>
+FOOTER
